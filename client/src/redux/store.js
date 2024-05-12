ae03bbfb4c80df4a05fcab1350f6cfd03cae0775
Min Paing <minmyatpaing64@gmail.com>
2024-05-12 09:27:15 +0100

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./slices/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    version: 1,
+  },
+  rootReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persister = persistStore(store);
