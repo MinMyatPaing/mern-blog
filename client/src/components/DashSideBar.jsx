199ab143ec3f8cf2cfd7f0a9c09a5884fd5256aa
Min Paing <minmyatpaing64@gmail.com>
2024-05-12 18:21:47 +0100

    implemented image upload feature

---
+++ client/src/components/DashSideBar.jsx
@@ -0,0 +1,34 @@
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSideBar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item as="div" active={tab === "profile"} icon={HiUser} label="User">
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item className="cursor-pointer" icon={HiArrowSmRight}>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import DashProfile from "../components/DashProfile";
+import DashSideBar from "../components/DashSideBar";
+
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSideBar />
+      </div>
+
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
