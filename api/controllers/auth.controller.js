a61687614f3466fb1e73ffb51d79f6ac6ce60ede
Min Paing <minmyatpaing64@gmail.com>
2024-05-12 08:58:40 +0100

    implemented sigin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import throwError from "../utils/throwError.js";
@@ -28,6 +29,38 @@ export const signup = async (req, res, next) => {
 
     await newUser.save();
     res.json({ message: "signed up successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+      throwError(404, "All fields are required");
+    }
+    console.log(email, password);
+    const validUser = await User.findOne({ email: email });
+
+    if (!validUser) {
+      throwError(404, "User not found!");
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      throwError(402, "Invalid password!");
+    }
+
+    const { password: _, ...rest } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
