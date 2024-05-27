da25d6a988a5fb82398163e65198586a97f81ff2
Min Paing <minmyatpaing64@gmail.com>
2024-05-27 01:57:44 +0100

    implemented get users api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { updateUserById, deleteUserById, signout } from "../controllers/user.controller.js";
+import { updateUserById, deleteUserById, signout, getUsers } from "../controllers/user.controller.js";
 import verifyUser from "../utils/verifyUser.js";
 
 const router = express.Router();
 
+router.get("/get-users", verifyUser, getUsers);
 router.put("/update/:userId", verifyUser, updateUserById);
 router.delete("/delete/:userId", verifyUser, deleteUserById);
 router.post("/signout", signout);
