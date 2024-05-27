38765ab13b13bfb24124c6afae44708a5f65f20b
Min Paing <minmyatpaing64@gmail.com>
2024-05-27 01:36:39 +0100

    implemented update post function

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 import verifyUser from "../utils/verifyUser.js";
-import { createPost, getPosts, deletePost } from "../controllers/post.controller.js";
+import { createPost, getPosts, deletePost, updatePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createPost);
 router.get("/get-posts", getPosts);
 router.delete('/delete-post/:postId/:userId', verifyUser, deletePost);
+router.put('/update-post/:postId/:userId', verifyUser, updatePost)
 
 export default router;
